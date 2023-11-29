91eb9490e9730e37c0948e867bcdcbad58fdb965
danbi <leedanbiii@naver.com>
2023-11-29 22:01:12 +0900

    컴포넌트 스캔

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,8 +1,10 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
-
+@Component
 public class MemoryMemberRepository implements MemberRepository {
 
     // HashMap<>은 동시성 이슈가 있지만 간단하게 개발 용도로 사용
